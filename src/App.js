f9c46a1e0d86b73689abb2aecf6d85c5e20d3483
Emre Kaan Çetin <20030146+EmKaCe@users.noreply.github.com>
2021-01-11 02:33:34 +0100

    added everything that is necessary

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import { createMuiTheme, CssBaseline, Grid, makeStyles, ThemeProvider, Typography } from "@material-ui/core";
+import { indigo } from "@material-ui/core/colors";
+import React from "react";
+import Wizard from "./Wizard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const useStyles = makeStyles(() => ({
+    root: {
+        margin: "5em 0"
+    }
+}));
+
+const App = () => {
+    const classes = useStyles();
+
+    const theme = createMuiTheme({
+        palette: {
+            common: {
+                black: "#000",
+                white: "#fff"
+            },
+            type: "dark",
+            primary: {
+                main: indigo["800"]
+            },
+            secondary: {
+                main: indigo["700"]
+            },
+            error: {
+                light: "#e57373",
+                main: "#f44336",
+                dark: "#d32f2f",
+                contrastText: "#fff"
+            },
+            warning: {
+                light: "#ffb74d",
+                main: "#ff9800",
+                dark: "#f57c00",
+                contrastText: "rgba(0, 0, 0, 0.87)"
+            },
+            info: {
+                light: "#64b5f6",
+                main: "#2196f3",
+                dark: "#1976d2",
+                contrastText: "#fff"
+            },
+            success: {
+                light: "#81c784",
+                main: "#4caf50",
+                dark: "#388e3c",
+                contrastText: "rgba(0, 0, 0, 0.87)"
+            },
+            grey: {
+                50: "#fafafa",
+                100: "#f5f5f5",
+                200: "#eeeeee",
+                300: "#e0e0e0",
+                400: "#bdbdbd",
+                500: "#9e9e9e",
+                600: "#757575",
+                700: "#616161",
+                800: "#424242",
+                900: "#212121",
+                A100: "#d5d5d5",
+                A200: "#aaaaaa",
+                A400: "#303030",
+                A700: "#616161"
+            },
+            text: {
+                primary: "#fff",
+                secondary: "rgba(255, 255, 255, 0.7)",
+                disabled: "rgba(255, 255, 255, 0.5)",
+                hint: "rgba(255, 255, 255, 0.5)",
+                icon: "rgba(255, 255, 255, 0.5)"
+            },
+            divider: "rgba(255, 255, 255, 0.12)",
+            background: {
+                paper: "#424242",
+                default: "#303030"
+            },
+            action: {
+                active: "#fff",
+                hover: "rgba(255, 255, 255, 0.08)",
+                hoverOpacity: 0.08,
+                selected: "rgba(255, 255, 255, 0.16)",
+                selectedOpacity: 0.16,
+                disabled: "rgba(255, 255, 255, 0.3)",
+                disabledBackground: "rgba(255, 255, 255, 0.12)",
+                disabledOpacity: 0.38,
+                focus: "rgba(255, 255, 255, 0.12)",
+                focusOpacity: 0.12,
+                activatedOpacity: 0.24
+            }
+        }
+    });
+
+    return (
+        <div>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Grid
+                    className={classes.root}
+                    container
+                    direction="column"
+                    justify="center"
+                    alignItems="center"
+                >
+                    <Typography variant="h2">[Belastend] Modpack Tutorial</Typography>
+                    <Wizard />
+                </Grid>
+            </ThemeProvider>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/Wizard.js
@@ -0,0 +1,408 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from "react";
+import { Box, Button, ButtonGroup, Grid, IconButton, Link, makeStyles, Snackbar, TextField, Typography } from "@material-ui/core";
+import { Add, ArrowBack, ArrowForward, ArrowRightAlt, GetApp } from "@material-ui/icons";
+import { Alert } from "@material-ui/lab";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        margin: "3em 0",
+        padding: "3em",
+        backgroundColor: theme.palette.background.paper,
+        borderRadius: "10px"
+    },
+    stepInstruction: {
+        marginBottom: "1em",
+        textAlign: "center"
+    },
+    launcherBox: {
+        position: "relative",
+        marginLeft: "0%"
+    },
+    loginContainer: {
+        height: "100%"
+    },
+    loginInput: {
+        margin: "0.75rem"
+    },
+    addButton: {
+        position: "absolute",
+        borderRadius: "0",
+        backgroundColor: theme.palette.primary.dark,
+        bottom: "25px",
+        left: "25px",
+        width: "50px",
+        height: "40px"
+    },
+    instanceTypeBox: {
+        position: "absolute",
+        top: "50px",
+        left: "200px"
+    },
+    instanceInput: {
+        position: "absolute",
+        width: "500px",
+        top: "200px",
+        left: "100px",
+    },
+    instanceButton: {
+        position: "absolute",
+        borderRadius: "0",
+        width: "70px",
+        height: "50px",
+        bottom: "25px",
+        right: "25px"
+    },
+    launchInstanceBox: {
+        position: "absolute",
+        top: "100px",
+        left: "50px",
+        borderRadius: "5px",
+        cursor: "pointer"
+    },
+    stepButtons: {
+        marginTop: "2em"
+    },
+    connectText: {
+        fontSize: "20px"
+    }
+}));
+
+const Wizard = () => {
+    const classes = useStyles();
+
+    // eslint-disable-next-line no-unused-vars
+    const [currentStep, setCurrentStep] = React.useState(0);
+    const [copied, setCopied] = React.useState(false);
+
+    return (
+        <div className={classes.root}>
+            <Grid
+                direction="column"
+                justify="center"
+                alignItems="center"
+            >
+                {currentStep == 0 ? (
+                    <div>
+                        <Typography
+                            className={classes.stepInstruction}
+                            variant="h4"
+                        >
+                            Installiere und Starte den GDLauncher.
+                        </Typography>
+                        <Grid
+                            className={classes.stepInstruction}
+                            container
+                            direction="row"
+                            justify="space-evenly"
+                            alignItems="center"
+                        >
+                            <Link href="https://github.com/gorilla-devs/GDLauncher/releases/download/v1.0.11/GDLauncher-win-setup.exe" rel="noreferrer" target="_blank">
+                                <Button
+                                    variant="contained"
+                                    color="secondary"
+                                    size="large"
+                                    startIcon={<GetApp />}
+                                >
+                                    Windows
+                                </Button>
+                            </Link>
+                            <Link href="https://github.com/gorilla-devs/GDLauncher/releases/download/v1.0.11/GDLauncher-mac-setup.dmg" rel="noreferrer" target="_blank">
+                                <Button
+                                    variant="contained"
+                                    color="secondary"
+                                    size="large"
+                                    startIcon={<GetApp />}
+                                >
+                                    Mac OS
+                                </Button>
+                            </Link>
+                            <Link href="https://github.com/gorilla-devs/GDLauncher/releases/download/v1.0.11/GDLauncher-linux-setup.AppImage" rel="noreferrer" target="_blank">
+                                <Button
+                                    variant="contained"
+                                    color="secondary"
+                                    size="large"
+                                    startIcon={<GetApp />}
+                                >
+                                    Linux
+                                </Button>
+                            </Link>
+                        </Grid>
+                        <Typography
+                            align="center"
+                            variant="body1"
+                        >
+                            Probleme bei der Installation?&nbsp;
+                            <Link
+                                href="https://github.com/gorilla-devs/GDLauncher/releases/download/v1.0.11/GDLauncher-win-portable.zip"
+                                rel="noreferrer"
+                                target="_blank"
+                                color="inherit"
+                                style={{ textDecoration: "underline" }}
+                            >
+                                Probiere es mit der portablen Version!
+                            </Link>
+                        </Typography>
+                    </div>
+                ) : null}
+                {currentStep == 1 ? (
+                    <Grid
+                        container
+                        direction="column"
+                        justify="center"
+                        alignItems="center"
+                    >
+                        <Typography
+                            className={classes.stepInstruction}
+                            variant="h4"
+                        >
+                            Logge dich mit deinen Minecraft Account-Daten ein.
+                        </Typography>
+                        <Box
+                            className={classes.launcherBox}
+                            key="launcherBox"
+                            width={700}
+                            height={500}
+                            bgcolor="secondary.main"
+                        >
+                            <Box
+                                key="loginBox"
+                                width={200}
+                                height={500}
+                                bgcolor="primary.main"
+                            >
+                                <Grid
+                                    className={classes.loginContainer}
+                                    container
+                                    direction="column"
+                                    justify="center"
+                                    alignItems="center"
+                                >
+                                    <TextField
+                                        className={classes.loginInput}
+                                        label="E-Mail"
+                                        variant="outlined"
+                                        disabled={true}
+                                    />
+                                    <TextField
+                                        className={classes.loginInput}
+                                        label="Passwort"
+                                        variant="outlined"
+                                        disabled={true}
+                                    />
+                                </Grid>
+                            </Box>
+                        </Box>
+                    </Grid>
+                ) : null}
+                {currentStep == 2 ? (
+                    <Grid
+                        container
+                        direction="column"
+                        justify="center"
+                        alignItems="center"
+                    >
+                        <Typography
+                            className={classes.stepInstruction}
+                            variant="h4"
+                        >
+                            Klicke unten Links auf das "+"-Symbol
+                        </Typography>
+                        <Box
+                            className={classes.launcherBox}
+                            key="launcherBox"
+                            width={700}
+                            height={500}
+                            bgcolor="secondary.main"
+                        >
+                            <IconButton
+                                className={classes.addButton}
+                                color="initial"
+                                onClick={() => setCurrentStep(currentStep + 1)}
+                            >
+                                <Add />
+                            </IconButton>
+                        </Box>
+                    </Grid>
+                ) : null}
+                {currentStep == 3 ? (
+                    <Grid
+                        container
+                        direction="column"
+                        justify="center"
+                        alignItems="center"
+                    >
+                        <Typography
+                            className={classes.stepInstruction}
+                            variant="h4"
+                        >
+                            Klicke oben auf "Import Zip"
+                        </Typography>
+                        <Box
+                            className={classes.launcherBox}
+                            key="launcherBox"
+                            width={700}
+                            height={500}
+                            bgcolor="secondary.main"
+                        >
+                            <ButtonGroup
+                                className={classes.instanceTypeBox}
+                                color="primary"
+                                variant="contained"
+                            >
+                                <Button disabled>Vanilla</Button>
+                                <Button disabled>Twitch</Button>
+                                <Button onClick={() => setCurrentStep(currentStep + 1)}>Import Zip</Button>
+                            </ButtonGroup>
+                        </Box>
+                    </Grid>
+                ) : null}
+                {currentStep == 4 ? (
+                    <Grid
+                        container
+                        direction="column"
+                        justify="center"
+                        alignItems="center"
+                    >
+                        <div className={classes.stepInstruction}>
+                            <Typography variant="h4">Füge den Link ein und klicke auf den Pfeil unten rechts.</Typography>
+                            <Typography variant="h6">Tipp: Wenn du den Link anklickst, dann kopiert er sich von alleine</Typography>
+                        </div>
+                        <Box
+                            className={classes.launcherBox}
+                            key="launcherBox"
+                            width={700}
+                            height={500}
+                            bgcolor="secondary.main"
+                        >
+                            <ButtonGroup
+                                className={classes.instanceTypeBox}
+                                color="primary"
+                                variant="contained"
+                            >
+                                <Button disabled>Vanilla</Button>
+                                <Button disabled>Twitch</Button>
+                                <Button>Import Zip</Button>
+                            </ButtonGroup>
+                            <TextField
+                                className={classes.instanceInput}
+                                variant="outlined"
+                                value="https://emkace.de/files/Belastend-1.16.4.zip"
+                                inputProps={{
+                                    readOnly: true
+                                }}
+                                onClick={(e) => {
+                                    e.target.focus();
+                                    e.target.select();
+                                    document.execCommand("copy");
+                                    document.getSelection().removeAllRanges();
+                                    setCopied(true);
+                                }}
+                            />
+                            <IconButton
+                                className={classes.instanceButton}
+                                onClick={() => setCurrentStep(currentStep + 1)}
+                            >
+                                <ArrowRightAlt fontSize="large" />
+                            </IconButton>
+                        </Box>
+                        <Snackbar open={copied} autoHideDuration={6000} onClose={() => setCopied(false)}>
+                            <Alert onClose={() => setCopied(false)} severity="success" color="info">
+                                Adresse kopiert! Mit STRG+V einfügen
+                            </Alert>
+                        </Snackbar>
+                    </Grid>
+                ) : null}
+                {currentStep == 5 ? (
+                    <Grid
+                        container
+                        direction="column"
+                        justify="center"
+                        alignItems="center"
+                    >
+                        <div className={classes.stepInstruction}>
+                            <Typography variant="h4">Du kannst das Modpack nun starten!</Typography>
+                            <Typography variant="h6">Dafür musst du einfach nur die "Belastend" Instanz anklicken</Typography>
+                        </div>
+                        <Box
+                            className={classes.launcherBox}
+                            key="launcherBox"
+                            width={700}
+                            height={500}
+                            bgcolor="secondary.main"
+                        >
+                            <Box
+                                className={classes.launchInstanceBox}
+                                key="launchInstanceBox"
+                                width={180}
+                                height={100}
+                                bgcolor="primary.main"
+                                onClick={() => setCurrentStep(currentStep + 1)}
+                            >
+                                <Typography
+                                    variant="h6"
+                                    align="center"
+                                    style={{ padding: "20%" }}
+                                >
+                                    Belastend
+                                </Typography>
+                            </Box>
+                        </Box>
+                    </Grid>
+                ) : null}
+                {currentStep == 6 ? (
+                    <Grid
+                        container
+                        direction="column"
+                        justify="center"
+                        alignItems="center"
+                    >
+                        <div className={classes.stepInstruction}>
+                            <Typography variant="h4">Mit dem Server verbinden!</Typography>
+                            <Typography variant="h6">Stelle vorher sicher, dass du auf der Whitelist stehst.</Typography>
+                        </div>
+                        <Typography
+                            className={classes.connectText}
+                            align="center"
+                            variant="body1">
+                            <p>Nehme deine Einstellungen vor, wie es dir beliebt und<br />
+                            installiere auch gerne ein Resource Pack oder Optifine, falls du das möchtest.</p>
+                            <p>Die Server-Adresse lautet:<br />
+                                <code>mc.emkace.de</code>
+                            </p>
+                            <p>Ich wünsche dir viel Spaß!</p>
+                        </Typography>
+                    </Grid>
+                ) : null}
+                <Grid
+                    className={classes.stepButtons}
+                    container
+                    direction="row"
+                    justify="space-between"
+                    alignItems="center"
+                >
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        disabled={currentStep == 0}
+                        startIcon={<ArrowBack />}
+                        onClick={() => setCurrentStep(currentStep - 1)}
+                    >
+                        Zurück
+                    </Button>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        disabled={currentStep == 6}
+                        endIcon={<ArrowForward />}
+                        onClick={() => setCurrentStep(currentStep + 1)}
+                    >
+                        Weiter
+                    </Button>
+                </Grid>
+            </Grid>
+        </div>
+    );
+};
+
+export default Wizard;
